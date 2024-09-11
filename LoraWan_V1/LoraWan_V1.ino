da3cdf5f403105e4f8ed017b286a70a73ac8b695
Remiker <jadroysfeir@hotmail.com>
2024-09-11 17:49:32 +0200

    Reduced temperature data sent

--- LoraWan_V1/LoraWan_V1.ino
+++ LoraWan_V1/LoraWan_V1.ino
@@ -1,4 +1,4 @@
-#include <MKRWAN.h>
+/*#include <MKRWAN.h>
 #include "arduino_secrets.h"
 #include <Arduino_MKRENV.h>
 #include <SPI.h>
@@ -9,7 +9,7 @@ int counter = 0;
 String appEui = SECRET_APP_EUI;
 String appKey = SECRET_APP_KEY;
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial);
   Serial.println("LoRa Sender");
 
@@ -33,14 +33,15 @@ delay(1000);
 }
 
 void loop() {
+  delay(5000);
   Serial.print("Sending packet: ");
   Serial.println(counter);
     double temperature = ENV.readTemperature();
   // send packet
-  LoRa.beginPacket();
-  LoRa.print(temperature);
-  LoRa.print(" celsius");
-  LoRa.endPacket();
+  //LoRa.beginPacket();
+  //LoRa.print(temperature);
+  //LoRa.print(" celsius");
+  //LoRa.endPacket();
 
 int err;
   modem.beginPacket();
@@ -53,12 +54,121 @@ int err;
     Serial.println("(you may send a limited amount of messages per minute, depending on the signal strength");
     Serial.println("it may vary from 1 message every couple of seconds to 1 message every minute)");
   }
-  //delay(1000);
+  delay(1000);
   if (!modem.available()) {
     Serial.println("No downlink message received at this time.");
     return;
   }
 
   counter++;
-  delay(60*1000);
+  delay(70000);
+}*/
+/*
+  Lora Send And Receive
+  This sketch demonstrates how to send and receive data with the MKR WAN 1300/1310 LoRa module.
+  This example code is in the public domain.
+*/
+
+#include <MKRWAN.h>
+#include <Arduino_MKRENV.h>
+#include <LoRa.h>
+LoRaModem modem;
+
+// Uncomment if using the Murata chip as a module
+// LoRaModem modem(Serial1);
+
+#include "arduino_secrets.h"
+// Please enter your sensitive data in the Secret tab or arduino_secrets.h
+String appEui = SECRET_APP_EUI;
+String appKey = SECRET_APP_KEY;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  // change this to your regional band (eg. US915, AS923, ...)
+  if (!modem.begin(EU868)) {
+    Serial.println("Failed to start module");
+    while (1) {}
+  };
+  Serial.print("Your module version is: ");
+  Serial.println(modem.version());
+  Serial.print("Your device EUI is: ");
+  Serial.println(modem.deviceEUI());
+
+  int connected = modem.joinOTAA(appEui, appKey);
+  if (!connected) {
+    Serial.println("Something went wrong; are you indoor? Move near a window and retry");
+    while (1) {}
+  }
+
+  // Set poll interval to 60 secs.
+  modem.minPollInterval(60);
+  // NOTE: independent of this setting, the modem will
+  // not allow sending more than one message every 2 minutes,
+  // this is enforced by firmware and can not be changed.
+  delay(1000);
+
+  if (!ENV.begin()) {
+    Serial.println("Failed to initialize MKR ENV shield!");
+    while (1)
+      ;
+  }
+}
+
+void loop() {
+  Serial.println();
+  //Serial.println("Enter a message to send to network");
+  //Serial.println("(make sure that end-of-line 'NL' is enabled)");
+
+  //while (!Serial.available());
+  //String msg ="";
+
+  Serial.println();
+  //  Serial.print("Sending: " + msg + " - ");
+  /*for (unsigned int i = 0; i < msg.length(); i++) {
+    Serial.print(msg[i] >> 4, HEX);
+    Serial.print(msg[i] & 0xF, HEX);
+    Serial.print(" ");
+  }*/
+  Serial.println();
+  char buffers[10];
+  uint16_t temperature = ENV.readTemperature() * 100;
+  buffers[0] = byte(temperature >> 8);
+  buffers[1] = byte(temperature && 0xFF00);
+  int err;
+  modem.beginPacket();
+
+  modem.write(buffers, 2);
+  // modem.print(temperature,HEX);
+  // modem.print(temperature);
+  Serial.print("temperature : ");
+  Serial.println(temperature);
+  err = modem.endPacket(true);
+  if (err > 0) {
+    Serial.println("Message sent correctly!");
+    delay(10000);
+  } else {
+    Serial.println("Error sending message :(");
+    Serial.println("(you may send a limited amount of messages per minute, depending on the signal strength");
+    Serial.println("it may vary from 1 message every couple of seconds to 1 message every minute)");
+  }
+  delay(1000);
+  if (!modem.available()) {
+    Serial.println("No downlink message received at this time.");
+    return;
+  }
+  char rcv[64];
+  int i = 0;
+  while (modem.available()) {
+    rcv[i++] = (char)modem.read();
+  }
+  Serial.print("Received: ");
+  for (unsigned int j = 0; j < i; j++) {
+    Serial.print(rcv[j] >> 4, HEX);
+    Serial.print(rcv[j] & 0xF, HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
 }
