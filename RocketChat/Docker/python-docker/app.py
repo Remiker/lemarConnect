bebc5e1b7fab25655f97052aaadfc6cd30bbc015
Remiker <jadroysfeir@hotmail.com>
2024-12-16 11:19:58 +0100

    adding comments to the code

--- RocketChat/Docker/python-docker/app.py
+++ RocketChat/Docker/python-docker/app.py
@@ -12,14 +12,13 @@ import paho.mqtt.client as mqtt
 import json
 import asyncio
 
-
-# token = os.environ.get("INFLUXDB_TOKEN")
+# Define token, org, url and bucket for influxDB after creating a user on influxDB server
 token = "dbpeD9hPjiWm4afsX5fEe0lUpAI6QtTeiXmkpTxrQgwfY58TzWscagn0Sr_M6jiD8kl-A3ZF-PA0LcRP2pLlNg=="
 org = "IUT_Brest"
 url = "http://influxdb_docker:8086"
+bucket="lemarconnect"
 write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
-bucket="lemarconnect"
 write_api =  write_client.write_api(write_options=SYNCHRONOUS)
     
 
@@ -28,7 +27,7 @@ broker = "eu1.cloud.thethings.network"
 port = 1883
 topic = "v3/lc-test@ttn/devices/lc-algue/up"
 
-#topic = "v3/+/devices/+/up"
+#topic = "v3/+/devices/+/up" # gets all the devices on the account
 username = "lc-test@ttn"  # Replace with your username
 password = "NNSXS.W7SM22UBUMSCDHB6CVEK5IDSH6RSFOUNWNRKUZQ.SZRQNBQNMLUZU343R7Q7EWJ4XQPAO66LUSSUFLI5WJWLM5GM7YPQ"  # Replace with your password
 # Define the callback function for connection
@@ -41,17 +40,15 @@ def on_connect(clientmqtt, userdata, flags, rc):
         print("Failed to connect. Result code:", rc)
 
 
-decoded_payload = {"value": None}
-#decoded_payload = 0
+decoded_payload = {"value": None} # initialize the variable globaly to be used later
 # Define the callback function for message receipt
-message_id = 0
 # Callback for receiving MQTT messages
 def on_message(client, userdata, msg):
     global message_id
     try:
         # Parse the MQTT message
         x = json.loads(msg.payload.decode("utf-8"))
-        decoded_payload["value"] = x["uplink_message"]["decoded_payload"]["humidity"]
+        decoded_payload["value"] = x["uplink_message"]["decoded_payload"]["humidity"] # get the value of humidity only
         print(f"Decoded Payload: {decoded_payload['value']}")
     except Exception as e:
         print(f"Error processing message: {e}")
@@ -67,7 +64,7 @@ clientmqtt.on_message = on_message
 
 # Connect to the MQTT broker
 clientmqtt.connect(broker, port,10)
-# Loop to maintain the connection and handle events
+# Loop to maintain the connection and handle events, it starts on different thread so it doesn't block the code
 clientmqtt.loop_start()
 
 
@@ -92,14 +89,15 @@ async def root():
                 .tag("source", "test")
                 .field("counter", value)
             )
-            time.sleep(1) # separate points by 1 second
-            value += 1
+            value +=1
+            time.sleep(5) # separate points by 5 second
+    # query to receive the values that have been registered on influxdb within the last 30 minutes, on point "measurement1"
     write_api.write(bucket=bucket, org=org, record=point)
     query_api = write_client.query_api()
     query = """from(bucket: "lemarconnect")
     |> range(start: -30m)
     |> filter(fn: (r) => r._measurement == "measurement1")"""
-    tables = query_api.query(query, org="IUT_Brest")
+    tables = query_api.query(query, org=org) 
 
     for table in tables:
         for record in table.records:
@@ -109,7 +107,7 @@ async def root():
     
     if decoded_payload["value"] is None:
         return {"message": "No payload received yet"}
-    return {"decoded_payload": decoded_payload["value"]}
+    return {"decoded_payload": decoded_payload["value"]} # display the value on python_docker server
 
 
 #################### to be used later ################
